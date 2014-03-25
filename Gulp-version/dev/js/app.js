cec2c3dedcd2f6868f74c4d1ad6e716e761102e9
Amin Meyghani <amin.meyghani@quovo.com>
2014-03-25 19:39:18 -0400

    #1: trying to set up the views and the routings

--- Gulp-version/dev/js/app.js
+++ Gulp-version/dev/js/app.js
@@ -12,7 +12,20 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/components', {templateUrl: 'pages/all/app/app-components.html', controller: 'MyCtrl1'});
   $routeProvider.when('/mixins', {templateUrl: 'pages/all/app/app-mixins.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/docs/mixins', {templateUrl: 'pages/all/app/app-mixins.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/docs', {templateUrl: 'pages/main/docs.html', controller: 'MyCtrl2'});
+
   $routeProvider.when('/utils', {templateUrl: 'pages/all/_/_globals/_utils.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/demos/demo1', {templateUrl: 'pages/main/demo1.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/x', { templateUrl:function(params) {
+      // choose lastModule or defaultModule if none specified
+      debugger;
+      
+      return 'pages/main/demo1.html';
+    }});
   $routeProvider.when('/toc', {templateUrl: 'toc.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/toc'});
 }]);
+
+
+
